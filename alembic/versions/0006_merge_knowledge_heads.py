891b8e88ad9b27ac7764a7c16369d6e4178b98c1
qiumingc028-beep <qiumingc028@gmail.com>
2026-07-02 18:17:49 +0800

    fix: add knowledge merge migration

---
+++ alembic/versions/0006_merge_knowledge_heads.py
@@ -0,0 +1,20 @@
+"""merge knowledge heads
+
+Revision ID: 0006_merge_knowledge_heads
+Revises: 0005_knowledge_center_tables
+Create Date: 2026-07-02
+"""
+
+
+revision = "0006_merge_knowledge_heads"
+down_revision = "0005_knowledge_center_tables"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
